477ed0d8908f0b15517c1bead56a05badc87351f
Adriano Mariot <adrianomariot@hotmail.com>
2022-06-02 10:14:29 -0300

    Primeiro envio do Arquivo index.js atividade Material digital

---
+++ index.js
@@ -0,0 +1,10 @@
+// variável, seu valor pode ser alterado no decorrer do programa
+let nomeEstudante = "Helena";
+console.log(nomeEstudante);
+
+// constante, seu valor não pode ser alterado
+const nomeDoEstudante = "Helena";
+console.log(nomeDoEstudante);
+
+//Em outras linguagens, portanto, você encontrará algo semelhante a:
+String: nomeEstudante = "Helena";
