d67907e8660183fdac45098003bfe1183e6dad55
Adriano Mariot <adrianomariot@hotmail.com>
2022-06-02 10:30:12 -0300

    Exercicio 2 material digital

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// variável, seu valor pode ser alterado no decorrer do programa
-let nomeEstudante = "Helena";
-console.log(nomeEstudante);
-
-// constante, seu valor não pode ser alterado
-const nomeDoEstudante = "Helena";
-console.log(nomeDoEstudante);
-
-//Em outras linguagens, portanto, você encontrará algo semelhante a:
-String: nomeEstudante = "Helena";
+let listaDeEstudantes = ["Helena", "Chico", "Mário"];
+let quantidadeDeEstudantes = listaDeEstudantes.length;
+console.log(quantidadeDeEstudantes);
